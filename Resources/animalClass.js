109179092a4e381cf0c3b2d334e785f3a5f6ea1b
KofoCole <kofodym07@yahoo.com>
2023-09-08 17:53:06 +0100

    Create a class animal, with properties for species and sound.

---
+++ Resources/animalClass.js
@@ -0,0 +1,8 @@
+//A class Animal
+class Animal {
+  // Animal class with properties for species and sound.
+  constructor(species, sound) {
+    this.species = species;
+    this.sound = sound;
+  }
+}
